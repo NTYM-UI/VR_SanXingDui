03d28a6175ca1c59a19cd120198bf54b6ae15c40
NTYM-UI <2789794681@qq.com>
2025-04-15 21:44:45 +0800

    1

--- Assets/Scripts/开始场景/ObjectVisibilityController.cs
+++ Assets/Scripts/开始场景/ObjectVisibilityController.cs
@@ -2,21 +2,30 @@ using UnityEngine;
 
 public class ObjectVisibilityController : MonoBehaviour
 {
-    // 需要控制的两个物体
-    public GameObject object1;
-    public GameObject object2;
+    // 使用数组存储需要控制的物体
+    public GameObject[] controlledObjects;
 
     // 动画开始时的触发方法
     public void OnAnimationStart()
     {
-        object1.SetActive(false);
-        object2.SetActive(false);
+        SetObjectsActive(false);
     }
 
     // 动画结束时的触发方法
     public void OnAnimationEnd()
     {
-        object1.SetActive(true);
-        object2.SetActive(true);
+        SetObjectsActive(true);
+    }
+
+    // 统一控制物体激活状态的私有方法
+    private void SetObjectsActive(bool isActive)
+    {
+        foreach (var obj in controlledObjects)
+        {
+            if (obj != null) // 安全校验
+            {
+                obj.SetActive(isActive);
+            }
+        }
     }
 }
