054776f1be49af9226ef7550e102f4684df12e26
NTYM-UI <2789794681@qq.com>
2025-04-16 19:47:07 +0800

    删除开始自动索引开始场景

--- Assets/Scripts/切换场景/ChangeSceneName.cs
+++ Assets/Scripts/切换场景/ChangeSceneName.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class ChangeSceneName : MonoBehaviour
 {
-    private void Start()
-    {
-        // 开始场景
-        SceneLoader.Instance.SetTargetScene("开始场景");
-        SceneLoader.Instance.ChangeScene();
-    }
     public void KongBaiScene()
     {
         SceneLoader.Instance.ChangeScene("空白场景");
